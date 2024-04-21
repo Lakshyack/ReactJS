a6b35e6767ec056f5192cea799fc816ee00fa289
Lakshya <lakshyak7370@gmail.com>
2024-04-21 19:36:45 +0530

    Concept of import and export in js

--- temp1.mjs
+++ temp1.mjs
@@ -1,2 +1,6 @@
-import lak from './temp2.mjs'
+import lak, {a,b,c} from './temp2.mjs'
+
 console.log(lak);
+console.log(a);
+console.log(b);
+console.log(c);
--- temp2.mjs
+++ temp2.mjs
@@ -1,3 +1,8 @@
 const a = "lakshya";
 const b = "Shrey";
+const c = "Vardan";
 export default b;
+export{a};
+export{b};
+export{c};
+
